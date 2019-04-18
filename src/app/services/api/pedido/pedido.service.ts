e2d1a5d7838cca009e104a0a6d734fc640b2b826
Jaime Neves <jaimebarrosoneves@gmail.com>
2019-04-18 01:18:17 -0400

    update pedido api

--- src/app/services/api/pedido/pedido.service.ts
+++ src/app/services/api/pedido/pedido.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '@env/environment';
-
 import { HttpClient} from '@angular/common/http';
-import { Pedido } from './pedido';
 
 const API_URL = environment.apiUrl;
 
@@ -18,23 +16,6 @@ export class PedidoService {
       this.http.get(API_URL + '/api/unknown')
         .subscribe(res => resolve(res));
     });
-  }
-
-  public createPedido(pedido: Pedido) {
-    // will use this.http.post()
-  }
-
-  public getPedidoById(pedidoId: number) {
-    // will use this.http.get()
-  }
-
-  // API: PUT /pedidos/:id
-  public updatePedido(pedido: Pedido) {
-    // will use this.http.put()
-  }
-
-  public deletePedidoById(pedidoId: number) {
-    // will use this.http.delete()
   }
 
 }
